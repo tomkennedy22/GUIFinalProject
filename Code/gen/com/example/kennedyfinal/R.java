6ba273e5252cc7bd8f32532456e0ca1d280dc556
Tom Kennedy <tkennedy@smu.edu>
2014-05-07 22:28:41 -0500

    Main pages constructed

--- Code/gen/com/example/kennedyfinal/R.java
+++ Code/gen/com/example/kennedyfinal/R.java
@@ -995,7 +995,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f050041;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1010,6 +1010,8 @@ containing a value of this type.
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int gridView=0x7f05003f;
+        public static final int guessButton=0x7f050040;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
@@ -1045,6 +1047,8 @@ containing a value of this type.
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
+        public static final int welcome=0x7f05003d;
+        public static final int welcomeGo=0x7f05003e;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1097,11 +1101,14 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int activity_main=0x7f030017;
-        public static final int fragment_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_show_board=0x7f030018;
+        public static final int avaliable_letter=0x7f030019;
+        public static final int fragment_show_board=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
+        public static final int show_board=0x7f0c0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1146,7 +1153,12 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int choose_letter=0x7f0a0010;
+        public static final int guessLetter=0x7f0a0014;
         public static final int hello_world=0x7f0a000e;
+        public static final int title_activity_show_board=0x7f0a0013;
+        public static final int welcome=0x7f0a0011;
+        public static final int welcomeGo=0x7f0a0012;
     }
     public static final class style {
         /** 
